8d01999cac338c93c94c536986adfc486d218f41
bulte <bulte@192.168.115.144>
2021-10-04 10:14:50 +0200

    depot exo 1

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,25 @@
+//buteau etienne, ,apprendre java, 29/09/21
+ 
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bulte
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();  
+        System.out.println("Au revoir "+ prenom);
+    }
+    
+}
